0283c3bd5979dafc17fc0e05329a32b46b941416
Administrator <aflisun4eg@gmail.com>
2022-10-11 21:23:42 +0300

    m m m m m add First exaple!

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+int a = 5;
+int b = 7;
+int max = a;
+int min = a;
+
+if (max < a)
+{
+    max = a;
+}
+else
+{
+    min = a;
+}
+if (max < b)
+{
+    max = b;
+}
+else
+{
+    min = b;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
