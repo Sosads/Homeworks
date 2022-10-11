175fcb7d34fd491510e1a32a3a55ce41de93abc5
Administrator <aflisun4eg@gmail.com>
2022-10-12 00:11:59 +0300

    Добавил задачу 4

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 5; 
+for (int x = 2; x < a; x++)       
+if (x%2==0)            
+Console.WriteLine(x);
+    
+int b = 8; 
+for (int y = 2; y < b; y++)       
+if (y%2==0)            
+Console.WriteLine(y);
