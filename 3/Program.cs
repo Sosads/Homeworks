362d9f2c55254e2b9abc84cf3a9f120db793e9da
Administrator <aflisun4eg@gmail.com>
2022-10-11 23:36:36 +0300

    Добавил задачу 3

---
+++ 3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Число чётное?");
+int a = 4;                             
+if (a % 2==0)
+{
+Console.WriteLine("Да");
+           
+}
+else
+{
+Console.WriteLine("Нет");
+}
+
+int b = -3;                             
+if (b % 2==0)
+{
+Console.WriteLine("Да");
+           
+}
+else
+{
+Console.WriteLine("Нет");
+}
+
+int c = 7;                             
+if (c % 2==0)
+{
+Console.WriteLine("Да");
+           
+}
+else
+{
+Console.WriteLine("Нет");
+}
